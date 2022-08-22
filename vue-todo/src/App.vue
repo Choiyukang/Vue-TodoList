65eacb0e5a41f4e432c80b2e6c6a030a009e1c47
ykchoi <choe_gang@naver.com>
2022-08-22 09:53:52 +0900

    mapGetters, mutations

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -19,7 +19,6 @@ import TodoHeader from './components/TodoHeader.vue'
 import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
-// import {mapGetters} from 'vuex'
 
 /*
 ES5
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
     <transition-group name="list" tag="ul">
-      <li v-for="(todoItem, index) in this.$store.getters.storedTodoItems" v-bind:key="todoItem.item" class="shadow">
-        <span class="checkBtn" v-on:click="toggleComplete(todoItem, index)" v-bind:class="{checkBtnCompleted: todoItem.completed}">
-          <i class="fa-solid fa-check"></i>  
+      <li
+        v-for="(todoItem, index) in this.storedTodoItems"
+        v-bind:key="todoItem.item"
+        class="shadow"
+      >
+        <span
+          class="checkBtn"
+          v-on:click="toggleComplete({todoItem, index})"
+          v-bind:class="{ checkBtnCompleted: todoItem.completed }"
+        >
+          <i class="fa-solid fa-check"></i>
         </span>
         <!-- <i class="fa-solid fa-check checkBtn" v-on:click="toggleComplete"></i> -->
-        <span v-bind:class="{textCompleted : todoItem.completed}">{{todoItem.item}}</span>
-        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+        <span v-bind:class="{ textCompleted: todoItem.completed }">{{
+          todoItem.item
+        }}</span>
+        <span class="removeBtn" v-on:click="removeTodo({ todoItem, index })">
           <i class="fa-solid fa-trash-can"></i>
         </span>
       </li>
@@ -16,56 +26,66 @@
 </template>
 
 <script>
-export default {
-  methods:{
-    removeTodo(todoItem,index){
-      this.$store.commit('removeOneItem',{todoItem, index});
-    //  this.$emit('removeItem',todoItem, index)
+import { mapGetters, mapMutations } from "vuex";
 
-    },
-    toggleComplete(todoItem, index){
-    //  this.$emit('toggleItem',todoItem, index)
-     this.$store.commit('toggleOneItem', {todoItem, index})
-    }
-  }
-}
+export default {
+  methods: {
+    ...mapMutations({
+      removeTodo: "removeOneItem",
+      toggleComplete: "toggleOneItem",
+    }),
+
+    // removeTodo(todoItem, index) {
+    //   this.$store.commit("removeOneItem", { todoItem, index });
+    //   //  this.$emit('removeItem',todoItem, index)
+    // },
+    // toggleComplete(todoItem, index) {
+    //   //  this.$emit('toggleItem',todoItem, index)
+    //   this.$store.commit("toggleOneItem", { todoItem, index });
+    // },
+  },
+  computed: {
+    ...mapGetters(["storedTodoItems"]),
+  },
+};
 </script>
 
 <style scoped>
-ul{
+ul {
   list-style-type: none;
   padding-left: 0px;
   margin-top: 0;
   text-align: center;
-  }
-  li{
-    display: flex;
-    min-height: 50px;
-    height: 50px;
-    line-height: 50px;
-    margin: 0.5rem 0;
-    padding:0 0.9rem;
-    background: white;
-    border-radius: 5px;
-  }
-  .checkBtn{
-    line-height: 45px;
-    color: #62acde;
-    margin-right: 5px;
-  }
-  .checkBtnCompleted{
-    color:#b3adad;
-  }
-  .textCompleted{
-    text-decoration: line-through;
-    color: #b3adad;
-  }
-  .removeBtn{
-    margin-left: auto;
-    color: #de4343;
-  }
-  /* listItem 트랜지션 효과 */
-.list-enter-active, .list-leave-active {
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+/* listItem 트랜지션 효과 */
+.list-enter-active,
+.list-leave-active {
   transition: all 1s;
 }
 .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
